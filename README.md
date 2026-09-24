597c1d06f76d455ef7aef7d3d2b3b7f05ad760b3
aaron-nanas <ananas2516@gmail.com>
2026-09-24 15:06:40 -0700

    Added template for the Line Follower lab

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# ECE_528L_Line_Follower
+# ECE 528/L - Robotics and Embedded Systems with Lab
+**CSU Northridge**
+
+**Department of Electrical and Computer Engineering**
+
+## Line Follower Lab
+The Line Follower lab interfaces with the following:
+
+* User LEDs of the TI MSP432 LaunchPad
+* Pololu Gearmotor with Encoder - [Product Link](https://www.pololu.com/product/3675)
+* 8-Channel QTRX Sensor Array - [Product Link](https://www.pololu.com/product/3672)
---
+++ ece528L_line_follower/Line_Follower/main.c
@@ -0,0 +1,31 @@
+/**
+ * @file main.c
+ *
+ * @brief Main source code for the Line_Follower program.
+ *
+ * This file contains the main entry point for the Line_Follower program.
+ * The main controller demonstrates a Line Follower robot without using an advanced algorithm.
+ *
+ * It interfaces the following peripherals using GPIO to demonstrate line following:
+ *  - 8-Channel QTRX Sensor Array module
+ *
+ * Timers are used in this lab:
+ *  - SysTick:  Used to generate periodic interrupts at a specified rate (1 kHz)
+ *  - Timer A0: Used to generate PWM signals that will be used to drive the DC motors
+ *  - Timer A1: Used to generate periodic interrupts at a specified rate (1 kHz)
+ *
+ * @note For more information regarding the 8-Channel QTRX Sensor Array module,
+ * refer to the product page: https://www.pololu.com/product/3672
+ *
+ * @author
+ *
+ */
+
+int main(void)
+{
+
+    while(1)
+    {
+
+    }
+}
